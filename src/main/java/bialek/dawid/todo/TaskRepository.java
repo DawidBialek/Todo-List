d4d6243260462b7c904881ef9e6b68bd8fa1bb0a
DawidBialek <dd.bialek@gmail.com>
2021-02-02 12:25:57 -0500

    Add modify and delete task tests

--- src/main/java/bialek/dawid/todo/TaskRepository.java
+++ src/main/java/bialek/dawid/todo/TaskRepository.java
@@ -2,6 +2,9 @@ package bialek.dawid.todo;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
 
+    boolean delete(Optional<Task> l);
 }
--- src/test/java/bialek/dawid/todo/HibernateTest.java
+++ src/test/java/bialek/dawid/todo/HibernateTest.java
@@ -14,7 +14,10 @@ import org.springframework.util.Assert;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
+import java.util.Optional;
 
+import static org.mockito.BDDMockito.given;
+import static org.mockito.Mockito.times;
 import static org.springframework.test.util.AssertionErrors.assertTrue;
 
 @SpringBootTest
@@ -24,50 +27,50 @@ class HibernateTest {
     private TaskRepository taskRepository;
     private List<Task> taskList;
 
-    @BeforeEach
-    public void setup(){
-        // Initialize Task mock objects for testing     
-        taskList = new ArrayList<Task>();
+//    @BeforeEach
+//    public void setup(){
+//        // Initialize Task mock objects for testing
+//        taskList = new ArrayList<Task>();
+//
+//        Task task1 = new Task();
+//        task1.setName("task1");
+//        task1.setPriority(1);
+//
+//        Task task2 = new Task();
+//        task2.setName("task2");
+//        task2.setPriority(2);
+//
+//        Task task3 = new Task();
+//        task3.setName("task3");
+//        task3.setPriority(5);
+//
+//        Task task4 = new Task();
+//        task4.setName("task4");
+//        task4.setPriority(1);
+//
+//        taskList.add(task1);
+//        taskList.add(task2);
+//        taskList.add(task3);
+//        taskList.add(task4);
+//    }
 
+    @Test
+    public void testSave(){
+        //given
         Task task1 = new Task();
         task1.setName("task1");
         task1.setPriority(1);
 
         Task task2 = new Task();
         task2.setName("task2");
-        task2.setPriority(2);
-
-        Task task3 = new Task();
-        task3.setName("task3");
-        task3.setPriority(5);
-
-        Task task4 = new Task();
-        task4.setName("task4");
-        task4.setPriority(1);
-
-        taskList.add(task1);
-        taskList.add(task2);
-        taskList.add(task3);
-        taskList.add(task4);
-    }
-
-    @Test
-    public void testSave(){
-        // given
-        Task task4 = new Task();
-        task4.setName("task4");
-        task4.setPriority(1);
-
-        Task task5 = new Task();
-        task4.setName("task5");
-        task4.setPriority(1);
+        task2.setPriority(1);
 
         TaskRepository taskRepository = Mockito.mock(TaskRepository.class);
-        Mockito.when(taskRepository.save(task4)).thenReturn(task5);
-
-        Task taskResult = taskRepository.save(task4);
-
-        Assertions.assertEquals(task5, taskResult);
+        given(taskRepository.save(task1)).willReturn(task2);
+        //when
+        Task taskResult = taskRepository.save(task1);
+        //then
+        Assertions.assertEquals(task2, taskResult);
 
 //
 //        for (Iterator iterator = taskList.iterator(); iterator.hasNext();) {
@@ -82,36 +85,55 @@ class HibernateTest {
     }
 
     @Test
-    public void testUpdate(){
-        // get a Task object from the repository
-        Task TaskObj = taskRepository.findAll().get(0);
+    public void testModifyTask(){
+        //given
+        Task task1 = new Task();
+        task1.setName("task1");
+        task1.setPriority(1);
 
-        // assert if its not null
-        Assertions.assertTrue(!TaskObj.getName().isEmpty());
+        Task task2 = new Task();
+        task2.setName("task2");
+        task2.setPriority(5);
 
-        // change the task name 
-        TaskObj.setName("task1-Changed");
+        TaskRepository taskRepository = Mockito.mock(TaskRepository.class);
+        given(taskRepository.save(task1)).willReturn(task2);
+        //when
+        task1.setPriority(5);
+        Task taskResult = taskRepository.save(task1);
+        //then
+        Assertions.assertEquals(task2, taskResult);
+    }
 
-        // update the Task object
-        taskRepository.save(TaskObj);
-
-        // assert the value changed id true
-        Assertions.assertEquals("task1-Changed", taskRepository.findAll().get(0).getName());
+    public boolean delete(Long id){
+        Optional<Task> byId = taskRepository.findById(id);
+        if (byId != null) {
+            taskRepository.delete(byId);
+        }
+        Optional<Task> removed = taskRepository.findById(id);
+        System.out.println(removed);
+        if (removed.isEmpty()) {
+            return false;
+        }
+        return true;
     }
 
     @Test
     public void testDelete(){
-        // get a CASTask object from the repository
-        Task TaskObjBeforeDel = taskRepository.findAll().get(0);
+        //given
+        Task task1 = new Task();
+        task1.setName("task1");
+        task1.setPriority(1);
 
-        // delete the Task object
-        taskRepository.delete(TaskObjBeforeDel);
+        Task task2 = new Task();
+        task2.setName("task2");
+        task2.setPriority(5);
 
-        // get a Task object from the repository
-        Task TaskObjAfterDel = taskRepository.findAll().get(0);
-
-        // get the Task object ID 
-        Assertions.assertNull(TaskObjAfterDel);
+        TaskRepository taskRepository = Mockito.mock(TaskRepository.class);
+        given(taskRepository.findById(1L)).willReturn(Optional.of(task2)).willReturn(null);
+        //when
+        boolean result = delete(1L);
+        //then
+        Assertions.assertEquals(false, result);
     }
 
     @Test
@@ -127,12 +149,33 @@ class HibernateTest {
 
     @Test
     public void testFindAll(){
-        // get all Task object from the repository 
+        // get all Task object from the repository
         List<Task> taskList = taskRepository.findAll();
 
         // check if it returns all records from DB
         Assertions.assertTrue(taskList.size() > 0);
         Assertions.assertEquals(taskList.size(), 4);
+    }
+
+    private List<Task> prepareMockTasksData(){
+        Task task1 = new Task();
+        task1.setName("task1");
+        task1.setPriority(1);
+
+        Task task2 = new Task();
+        task2.setName("task2");
+        task2.setPriority(5);
+
+        Task task3 = new Task();
+        task3.setName("task3");
+        task3.setPriority(6);
+
+        List<Task> taskList = new ArrayList();
+        taskList.add(task1);
+        taskList.add(task2);
+        taskList.add(task3);
+
+        return taskList;
     }
 
 }
